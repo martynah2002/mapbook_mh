3848f4b4328100235af7d3309369d9b87491720e
martynah2002 <martyna.hybner@gmail.com>
2025-04-07 10:35:57 +0200

    wip: simple menu option

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from utills.controller import get_user_info
 from utills.model import users
 
+
 def main():
-    get_user_info(users)
+    while True:
+        print('====================MENU=====================')
+        print('0 - Exit')
+        print('1 - Get user info')
+        print('=============================================')
+
+        choice = input('Enter your choice:')
+        if choice == '0': break
+        if choice == '1': get_user_info(users)
+
 
 if __name__ == '__main__':
     main()
