a955d6abeb1396d0a65144f668d0164329a59a81
martynah2002 <martyna.hybner@gmail.com>
2025-04-07 11:14:51 +0200

    wip: new menu option

--- utills/controller.py
+++ utills/controller.py
@@ -1,3 +1,9 @@
 def get_user_info(users_data:list[dict])->None:
     for user in users_data:
         print(f'Twój znajomy: {user["name"]} z miejscowości: {user["location"]} opublikował: {user["posts"]} postów.')
+
+def add_user(user_data:list[dict])->None:
+    tmp_name:str = input('podaj imię: ')
+    tmp_location:str = input('podaj miejsce: ')
+    tmp_posts:int = int(input('podaj liczbę postów: '))
+    user_data.append({'name': tmp_name, 'location': tmp_location, 'posts': tmp_posts })
