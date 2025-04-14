238df90d2b30529da38447503684f9b4c74df141
martynah2002 <martyna.hybner@gmail.com>
2025-04-14 09:20:31 +0200

    wip: new menu option: update user

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from utills.controller import get_user_info, add_user, remove_user
+from utills.controller import get_user_info, add_user, remove_user, update_user
 from utills.model import users
-
 
 
 def main():
@@ -10,6 +9,7 @@ def main():
         print('1 - Get user info')
         print('2 - Add user')
         print('3 - Remove user')
+        print('4 - Update user')
         print('=============================================')
 
         choice = input('Enter your choice:')
@@ -17,7 +17,8 @@ def main():
         if choice == '1': get_user_info(users)
         if choice == '2': add_user(users)
         if choice == '3': remove_user(users)
+        if choice == '4': update_user(users)
+
 
 if __name__ == '__main__':
     main()
-
